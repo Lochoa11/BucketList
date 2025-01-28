ddef70fbf6d6766d279f4962ebdf6f9530df353a
Lochoa11 <linochoa11690@gmail.com>
2025-01-28 16:29:16 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Lin Ochoa on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
